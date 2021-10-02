c1dfc95e2012b7f675032164e3b9390a9a37899a
Kasuromi <29818914+Kasuromi@users.noreply.github.com>
2021-10-03 01:46:55 +0300

    Base BepInEx Plugin

---
+++ GTFO-API/APILogger.cs
@@ -0,0 +1,26 @@
+﻿using BepInEx.Logging;
+
+namespace GTFO.API
+{
+    internal static class APILogger
+    {
+        private static readonly ManualLogSource _logger;
+
+        static APILogger()
+        {
+            _logger = new ManualLogSource("GTFO-API");
+            Logger.Sources.Add(_logger);
+        }
+        private static string Format(object msg) => msg.ToString();
+
+        public static void Info(object data) => _logger.LogMessage(Format(data));
+        public static void Verbose(object data)
+        {
+#if DEBUG
+            _logger.LogDebug(Format(data));
+#endif
+        }
+        public static void Debug(object data) => _logger.LogDebug(Format(data));
+        public static void Error(object data) => _logger.LogError(Format(data));
+    }
+}
---
+++ GTFO-API/EntryPoint.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx;
+using BepInEx.IL2CPP;
+using HarmonyLib;
+
+namespace GTFO.API
+{
+    [BepInPlugin("dev.gtfomodding.gtfo-api", "GTFO-API", VersionInfo.Version)]
+    public class EntryPoint : BasePlugin
+    {
+        public override void Load()
+        {
+            APILogger.Verbose($"GTFO API Loading");
+            m_Harmony = new Harmony("dev.gtfomodding.gtfo-api");
+            m_Harmony.PatchAll();
+        }
+
+        private Harmony m_Harmony;
+    }
+}
