3811876d23c25371f0fa00ffc661283f18cc93ea
Kasuromi <29818914+Kasuromi@users.noreply.github.com>
2021-10-24 23:01:04 +0300

    GameDataAPI & Event for GameDataInit

---
+++ GTFO-API/API/GameDataAPI.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GTFO.API.Attributes;
+using GTFO.API.Resources;
+
+namespace GTFO.API
+{
+    [API("GameData")]
+    public class GameDataAPI
+    {
+        /// <summary>
+        /// Status info for the <see cref="GameDataAPI"/>
+        /// </summary>
+        public static ApiStatusInfo Status => APIStatus.GameData;
+
+        static GameDataAPI()
+        {
+            Status.Created = true;
+            Status.Ready = true;
+        }
+
+        /// <summary>
+        /// Invoked when the game data has been fully initialized
+        /// </summary>
+        public static event Action OnGameDataInitialized;
+
+        internal static void InvokeGameDataInit() => OnGameDataInitialized?.Invoke();
+    }
+}
