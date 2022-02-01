298cbaa44a9b852867e8170b04608fd96e29610d
CaffeineBytes <anthonysebok@live.com>
2022-01-31 19:19:59 -0500

    Do not save to favorites file on weapon changes and set favorites to default on load.

---
+++ GTFO-API/Patches/GearManager_Patches.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using Gear;
+using HarmonyLib;
+
+namespace GTFO.API.Patches
+{
+    [HarmonyPatch(typeof(GearManager))]
+    internal class GearManager_Patches
+    {
+        [HarmonyPatch(nameof(GearManager.RegisterGearInSlotAsEquipped))]
+        [HarmonyPrefix]
+        public static bool RegisterGearInSlotAsEquipped_Prefix()
+        {
+            return false;
+        }
+
+        [HarmonyPatch(nameof(GearManager.RegisterBotGearInSlotAsEquipped))]
+        [HarmonyPrefix]
+        public static bool RegisterBotGearInSlotAsEquipped_Prefix()
+        {
+            return false;
+        }
+
+        [HarmonyPatch(nameof(GearManager.LoadOfflineGearDatas))]
+        [HarmonyPostfix]
+        public static void LoadOfflineGearDatas_Postfix()
+        {
+            //Force players to equip the defaults for the currently loaded mod
+            GearManager.FavoritesData = new GearFavoritesData();
+            GearManager.BotFavoritesData = new BotFavoritesData();
+        }
+    }
+}
