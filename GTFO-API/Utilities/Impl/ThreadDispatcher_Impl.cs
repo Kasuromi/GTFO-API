62d6b1612c8cab4dd3273c3c4f7db26ed51b2e42
Flowaria <wnsghgj1234@naver.com>
2022-02-09 00:29:18 +0900

    Added ThreadDispatcher

---
+++ GTFO-API/Utilities/Impl/ThreadDispatcher_Impl.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AssetShards;
+using UnhollowerBaseLib.Attributes;
+using UnityEngine;
+
+namespace GTFO.API.Utilities.Impl
+{
+    internal class ThreadDispatcher_Impl : MonoBehaviour
+    {
+        public ThreadDispatcher_Impl(IntPtr intPtr) : base(intPtr) { }
+
+        static ThreadDispatcher_Impl()
+        {
+            AssetShardManager.add_OnStartupAssetsLoaded((Action)OnAssetsLoaded);
+        }
+
+        [HideFromIl2Cpp]
+        private static void OnAssetsLoaded()
+        {
+            if (s_Instance == null)
+            {
+                GameObject dispatcher = new();
+                var dispatcherComp = dispatcher.AddComponent<ThreadDispatcher_Impl>();
+                dispatcher.name = "GTFO-API Thread Dispatcher";
+                dispatcher.hideFlags = HideFlags.HideAndDontSave;
+                GameObject.DontDestroyOnLoad(dispatcher);
+
+                s_Instance = dispatcherComp;
+            }
+        }
+
+        [HideFromIl2Cpp]
+        internal static void Enqueue(Action action)
+        {
+            lock (s_LoopQueue)
+            {
+                s_LoopQueue.Enqueue(action);
+            }
+        }
+
+        internal void Update()
+        {
+            lock (s_LoopQueue)
+            {
+                while (s_LoopQueue.Count > 0)
+                {
+                    s_LoopQueue.Dequeue()?.Invoke();
+                }
+            }
+        }
+
+        private static ThreadDispatcher_Impl s_Instance = null;
+        private static readonly Queue<Action> s_LoopQueue = new();
+    }
+}
---
+++ GTFO-API/Utilities/ThreadDispatcher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GTFO.API.Utilities.Impl;
+
+namespace GTFO.API.Utilities
+{
+    /// <summary>
+    /// Unity Main Thread Dispatcher
+    /// </summary>
+    public static class ThreadDispatcher
+    {
+        /// <summary>
+        /// Enqueue Action that want to excute on main unity thread
+        /// </summary>
+        /// <param name="action">Action to dispatch to main unity thread</param>
+        public static void Enqueue(Action action)
+        {
+            ThreadDispatcher_Impl.Enqueue(action);
+        }
+    }
+}
