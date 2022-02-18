681a1ebae586bbe5221fd64bb71fbb4ebd56e1bf
CaffeineBytes <anthonysebok@live.com>
2022-02-18 16:02:45 -0500

    Switch class to internal use only

--- GTFO-API/Resources/PersistentData.cs
+++ GTFO-API/Resources/PersistentData.cs
@@ -3,10 +3,8 @@ using Player;
 
 namespace GTFO.API.Resources
 {
-#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
-    public static class PersistentData
+    internal static class PersistentData
     {
         public static Dictionary<InventorySlot, string[]> BotFavorites;
     }
-#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
 }
