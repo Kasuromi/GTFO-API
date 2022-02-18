b52a586d777cc53a36b727b6e52b5fe5aaab90a1
CaffeineBytes <anthonysebok@live.com>
2022-02-18 12:35:36 -0500

    Do not save player favorites and patch bot favorites to save in game but not in file.

---
+++ GTFO-API/Patches/GearManager_Patches.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using Gear;
+using HarmonyLib;
+using Player;
+using UnhollowerBaseLib.Attributes;
+
+namespace GTFO.API.Patches
+{
+    [HarmonyPatch(typeof(GearManager))]
+    internal class GearManager_Patches
+    {
+        [HideFromIl2Cpp]
+        public static Dictionary<InventorySlot, string[]> BotFavorites { get; set; }
+
+        [HarmonyPatch(nameof(GearManager.Setup))]
+        [HarmonyWrapSafe]
+        [HarmonyPostfix]
+        public static void Setup_Postfix()
+        {
+            BotFavorites = new Dictionary<InventorySlot, string[]>
+            {
+                { InventorySlot.GearMelee, new string[4] },
+                { InventorySlot.GearStandard, new string[4] },
+                { InventorySlot.GearSpecial, new string[4] },
+                { InventorySlot.GearClass, new string[4] },
+                { InventorySlot.HackingTool, new string[4] }
+            };
+
+            GearManager.FavoritesData = new GearFavoritesData();
+        }
+
+        [HarmonyPatch(nameof(GearManager.RegisterBotGearInSlotAsEquipped))]
+        [HarmonyWrapSafe]
+        [HarmonyPrefix]
+        public static bool RegisterBotGearInSlotAsEquipped_Prefix(GearIDRange idRange, InventorySlot slot, int slotIndex)
+        {
+            BotFavorites[slot][slotIndex] = idRange.ToJSON();
+            return false;
+        }
+
+        [HarmonyPatch(nameof(GearManager.SaveFavoritesData))]
+        [HarmonyWrapSafe]
+        [HarmonyPrefix]
+        public static bool SaveFavoritesData_Prefix()
+        {
+            return false;
+        }
+    }
+}
---
+++ GTFO-API/Patches/PlayerBackpackManager_Patches.cs
@@ -0,0 +1,34 @@
+﻿using HarmonyLib;
+using Player;
+using SNetwork;
+
+namespace GTFO.API.Patches
+{
+    [HarmonyPatch(typeof(PlayerBackpackManager))]
+    internal class PlayerBackpackManager_Patches
+    {
+        [HarmonyPatch(nameof(PlayerBackpackManager.GiveFavoriteGearToBot))]
+        [HarmonyWrapSafe]
+        [HarmonyPrefix]
+        public static bool GiveFavoriteGearToBotPrefix(SNet_Player bot, ref bool __result)
+        {
+            var botIndex = bot.PlayerSlotIndex();
+
+            if (string.IsNullOrEmpty(GearManager_Patches.BotFavorites[InventorySlot.GearMelee][botIndex]))
+            {
+                PlayerBackpackManager.CopyGearToBot(bot, PlayerBackpackManager.LocalBackpack);
+            } 
+            else
+            {
+                PlayerBackpackManager.EquipBotGear(bot, GearManager_Patches.BotFavorites[InventorySlot.GearMelee][botIndex]);
+                PlayerBackpackManager.EquipBotGear(bot, GearManager_Patches.BotFavorites[InventorySlot.GearStandard][botIndex]);
+                PlayerBackpackManager.EquipBotGear(bot, GearManager_Patches.BotFavorites[InventorySlot.GearSpecial][botIndex]);
+                PlayerBackpackManager.EquipBotGear(bot, GearManager_Patches.BotFavorites[InventorySlot.GearClass][botIndex]);
+                PlayerBackpackManager.EquipBotGear(bot, GearManager_Patches.BotFavorites[InventorySlot.HackingTool][botIndex]);
+            }
+
+            __result = true;
+            return false;
+        }
+    }
+}
