4b21c34e6e7b0ca87cc4ae60bd12120654e36fe1
Kasuromi <29818914+Kasuromi@users.noreply.github.com>
2022-07-14 23:53:11 -0700

    Plugin Whitelist (Held hostage by Dak)

--- GTFO-API/Patches/GameDataInit_Patches.cs
+++ GTFO-API/Patches/GameDataInit_Patches.cs
@@ -1,20 +1,99 @@
-﻿using GameData;
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+using BepInEx.IL2CPP;
+using GameData;
 using GTFO.API.Impl;
 using GTFO.API.Resources;
 using HarmonyLib;
 using UnityEngine.Analytics;
+using _StringUtils = global::GTFO.API.Utilities.StringUtils;
 
 namespace GTFO.API.Patches
 {
     [HarmonyPatch(typeof(GameDataInit))]
     internal class GameDataInit_Patches
     {
+        private struct PluginWhitelistInfo
+        {
+            public string GUID;
+            public string Name;
+            public string Version;
+            public string Checksum;
+        }
+        private static PluginWhitelistInfo[] FetchPluginWhitelist()
+        {
+            // NOTE: I was forced to add this by Dak because of complaints 
+            // that GTFO-API disables progression by forcing the rundown id to 1
+            // This is here against my will. Let that be known.
+            using HttpClient httpClient = new();
+            using Stream stream = httpClient.GetStreamAsync($"https://raw.githubusercontent.com/GTFO-Modding/GTFO-API-PluginWhitelist/main/whitelist.txt").GetAwaiter().GetResult();
+            using StreamReader reader = new(stream);
+            string[] whitelistData = reader.ReadToEnd().Split('\n', StringSplitOptions.RemoveEmptyEntries);
+
+            PluginWhitelistInfo[] whitelist = new PluginWhitelistInfo[whitelistData.Length];
+
+            for (int i = 0; i < whitelist.Length; i++)
+            {
+                string[] info = whitelistData[i].Split(":");
+                whitelist[i] = new()
+                {
+                    GUID = info[0],
+                    Name = info[1],
+                    Version = info[2],
+                    Checksum = info[3].TrimEnd('\r')
+                };
+            }
+
+            return whitelist;
+        }
         [HarmonyPatch(nameof(GameDataInit.Initialize))]
         [HarmonyWrapSafe]
         [HarmonyPostfix]
         public static void Initialize_Postfix()
         {
             Analytics.enabled = false;
+
+            try
+            {
+                // NOTE: I was forced to add this by Dak because of complaints 
+                // that GTFO-API disables progression by forcing the rundown id to 1
+                // This is here against my will. Let that be known.
+                PluginWhitelistInfo[] whitelistInfo = FetchPluginWhitelist();
+                if (whitelistInfo.Length == 0) throw null;
+
+                foreach (var loadedPlugin in IL2CPPChainloader.Instance.Plugins.Values)
+                {
+                    if (loadedPlugin.Metadata.GUID == "dev.gtfomodding.gtfo-api") continue;
+
+                    using SHA256 digest = SHA256.Create();
+                    string input =
+                        $"{loadedPlugin.Metadata.GUID}|" +
+                        $"{loadedPlugin.Metadata.Version}|" +
+                        $"{loadedPlugin.Metadata.Name}|" +
+                        $"{Path.GetFileName(loadedPlugin.Location)}|" +
+                        $"{loadedPlugin.Instance.GetType().Assembly.FullName}";
+
+                    byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+                    digest.TransformBlock(inputBytes, 0, inputBytes.Length, null, 0);
+
+                    byte[] pluginData = File.ReadAllBytes(loadedPlugin.Location);
+
+                    digest.TransformFinalBlock(pluginData, 0, pluginData.Length);
+                    string checksum = _StringUtils.FromByteArrayAsHex(digest.Hash);
+
+                    if (!whitelistInfo.Any((x) =>
+                        x.Name == loadedPlugin.Metadata.Name &&
+                        x.GUID == loadedPlugin.Metadata.GUID &&
+                        x.Version == loadedPlugin.Metadata.Version.ToString() &&
+                        x.Checksum == checksum)) throw null;
+                }
+                goto final;
+            }
+            catch { }
 
             GameSetupDataBlock setupBlock = GameDataBlockBase<GameSetupDataBlock>.GetBlock(1);
             if (setupBlock?.RundownIdToLoad != 1)
@@ -46,6 +125,7 @@ namespace GTFO.API.Patches
                 }
             }
 
+final:
             GameDataAPI.InvokeGameDataInit();
 
             if (APIStatus.Network.Created) return;
---
+++ GTFO-API/Utilities/StringUtils.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace GTFO.API.Utilities
+{
+    public static unsafe class StringUtils
+    {
+        private static readonly uint[] _lookup32 = new Func<uint[]>(() =>
+        {
+            var result = new uint[256];
+            for (int i = 0; i < 256; i++)
+            {
+                string s = i.ToString("X2");
+                if (BitConverter.IsLittleEndian)
+                    result[i] = ((uint)s[0]) + ((uint)s[1] << 16);
+                else
+                    result[i] = ((uint)s[1]) + ((uint)s[0] << 16);
+            }
+            return result;
+        })();
+        private static readonly uint* _lookup32Ptr = (uint*)GCHandle.Alloc(_lookup32, GCHandleType.Pinned).AddrOfPinnedObject();
+
+        public static string FromByteArrayAsHex(byte[] bytes)
+        {
+            var result = new char[bytes.Length * 2];
+            fixed (byte* bytesPtr = bytes)
+            fixed (char* resultPtr = result)
+            {
+                uint* resultPtr2 = (uint*)resultPtr;
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    resultPtr2[i] = _lookup32Ptr[bytesPtr[i]];
+                }
+            }
+            return new string(result);
+        }
+    }
+}
